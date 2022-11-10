a492b70b693275bfb3b335444556119bf9e862f9
ScapularSteam <86719656+ScapularSteam@users.noreply.github.com>
2022-11-10 10:05:39 +0000

    Main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+var lightCounter;
+
+if(document.getElementById('lightMode').clicked == true)
+{
+    var lightCounter = lightCounter + 1
+    document.getElementById("lCounter").innerHTML = lightCounter
+}
