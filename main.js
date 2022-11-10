2d4da69f6aa118eac0a3ae1b81ef0198ea556e4b
ScapularSteam <86719656+ScapularSteam@users.noreply.github.com>
2022-11-10 21:48:31 +0000

    Making Changes to main.js

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ var lightCounter;
 
 if(document.getElementById('lightMode').clicked == true)
 {
-    lightCounter +=1
-    document.getElementById("lCounter").innerHTML = lightCounter
-}
+    lightCounter +=1;
+    document.getElementById("lCounter").innerHTML = lightCounter;
+};
